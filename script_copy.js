d308cf0dfd08e9fb3c638935bb6c2a60f8284888
user <chuprin.exceedteam@gmail.com>
2021-12-20 11:15:23 +0300

    Fixed mistakes

--- script_copy.js
+++ script_copy.js
@@ -3,28 +3,29 @@ let input = null;
 let inputValue = '';
 let activeEditTask = null;
 let editTask = null;
+const link = 'http://localhost:8000';
 
 window.onload = async function init() {
     input = document.getElementById('add-task');
     input.addEventListener('change', updateValue);
-    const response = await fetch ('http://localhost:8000/allTasks', {
+    const response = await fetch (link + '/allTasks', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
             'Access-Control-Allow-Origin': 'http://localhost:8080'
         },
     });
-    let result = await response.json();
+    const  result = await response.json();
     allTasks = result.data;
     render ();
 }
 
-updateValue = (event) => {
+const updateValue = (event) => {
     inputValue = event.target.value;
 } 
 
-onClickButton = async () => {
-    const resp = await fetch ('http://localhost:8000/createTask', {
+const onClickButton = async () => {
+    const resp = await fetch (link + '/createTask', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -35,81 +36,80 @@ onClickButton = async () => {
             isCheck: false,
         })
     });
-    let result = await resp.json();
-    allTasks.push (result);
-    input.value = '';
-    inputValue = '';
-    render ();
+    if (resp.status === 200) {
+        const result = await resp.json();
+        allTasks.push (result);
+        input.value = '';
+        inputValue = '';
+        render ();
+    } else {
+        alert('Упс, что-то пошло не так');
+    }
 }    
 
-render = () => {
+const render = () => {
     const content = document.getElementById('content');
     while (content.firstChild) {
         content.removeChild(content.firstChild)
     }
-    
-    allTasks.sort((a,b) => a.isCheck > b.isCheck ? 1 : a.isCheck < b.isCheck ? -1 : 0)
+    allTasks
+        .sort ((a, b) => {
+            if (a.name === b.name) return 0;
+            return (a.name > b.name) ? 1 : -1
+        })
+        .map((value, index) => {
+            const container = document.createElement('div');
+            content.appendChild(container);
+            container.className = "task";
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.checked = value.isCheck;
+            checkbox.className = 'check';
+            container.appendChild(checkbox)
+            checkbox.onchange = () => {doneTask(value, index)};
 
-    allTasks.map((value, index) => {
-        const container = document.createElement('div');
-        content.appendChild(container);
-        container.className = "task";
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.checked = value.isCheck;
-        checkbox.className = 'check';
-        container.appendChild(checkbox)
-        checkbox.onchange = function () {
-            doneTask(value, index)
-        };
-
-        if (value.isCheck === false) {
-            if(value._id === activeEditTask) {
-                const inputTask = document.createElement('input');
-                inputTask.type = 'text';
-                inputTask.value = value.text;
-                inputTask.addEventListener('change', updateTaskText);
-                inputTask.addEventListener('blur', doneEditTask);
-                container.appendChild(inputTask);
-                const imageDone = document.createElement('img');
-                imageDone.src = 'done.svg';
-                imageDone.onclick = function () {
-                    doneEditTask
-                }
-                container.appendChild(imageDone);
+            if (!value.isCheck === true) {
+                if(value._id === activeEditTask) {
+                    const inputTask = document.createElement('input');
+                    inputTask.type = 'text';
+                    inputTask.value = value.text;
+                    inputTask.addEventListener('change', updateTaskText);
+                    inputTask.addEventListener('blur', doneEditTask);
+                    container.appendChild(inputTask);
+                    const imageDone = document.createElement('img');
+                    imageDone.src = 'img/done.svg';
+                    imageDone.onclick = () => {doneEditTask()};
+                    container.appendChild(imageDone);
+                } else {
+                    const text = document.createElement('p');
+                    text.innerText = value.text;
+                    container.appendChild(text);
+                    const imageEdit = document.createElement('img');
+                    container.appendChild(imageEdit);
+                    imageEdit.src = "img/pencil.svg";
+                    imageEdit.onclick = () => {
+                        activeEditTask = value._id;
+                    render (); 
+                    }
+                    text.className = 'task';
+            } 
             } else {
                 const text = document.createElement('p');
                 text.innerText = value.text;
                 container.appendChild(text);
-                const imageEdit = document.createElement('img');
-                container.appendChild(imageEdit);
-                imageEdit.src = "pencil.svg";
-                imageEdit.onclick = function () {
-                    activeEditTask = value._id;
-                render (); 
-                }
-                text.className = 'task';
-        } 
-        } else {
-            const text = document.createElement('p');
-            text.innerText = value.text;
-            container.appendChild(text);
-            text.className = 'done-task';
-        };
-        const imageDelete = document.createElement('img');
-                imageDelete.src = "cross.svg";
-                imageDelete.onclick = function () {
-                    deleteTask(value);
-                 };
-                container.appendChild(imageDelete);
-    });
-    
+                text.className = 'done-task';
+            };
+            const imageDelete = document.createElement('img');
+                    imageDelete.src = "img/cross.svg";
+                    imageDelete.onclick = () => {deleteTask(value)};
+                    container.appendChild(imageDelete);
+        });
 }
 
-doneEditTask = async (event) => {
+const doneEditTask = async (event) => {
     text = event.target.value;
     console.log('doneedit', activeEditTask)
-    const resp = await fetch ('http://localhost:8000/updateTaskText', {
+    const resp = await fetch (link + '/updateTaskText', {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -124,10 +124,11 @@ doneEditTask = async (event) => {
     activeEditTask = null;
     render();
 }
-doneTask = async (value, index) => {
+
+const doneTask = async (value, index) => {
     allTasks[index].isCheck = !allTasks[index].isCheck;
     activeEditTask = value._id;
-    const resp = await fetch ('http://localhost:8000/updateTask', {
+    const resp = await fetch (link + '/updateTask', {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -138,7 +139,7 @@ doneTask = async (value, index) => {
             isCheck: allTasks[index].isCheck
         })
     });
-    const response = await fetch ('http://localhost:8000/allTasks', {
+    const response = await fetch (link + '/allTasks', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -151,12 +152,12 @@ doneTask = async (value, index) => {
     render ();
 }
 
-deleteTask = async (event) => {
-    const resp = await fetch (`http://localhost:8000/deleteTask?_id=${event._id}`, {
+const deleteTask = async (event) => {
+    const resp = await fetch (link + `/deleteTask?_id=${event._id}`, {
         method: 'DELETE'
     });
     activeEditTask = null;
-    const response = await fetch ('http://localhost:8000/allTasks', {
+    const response = await fetch (link + '/allTasks', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -168,8 +169,8 @@ deleteTask = async (event) => {
     render();
 }
 
-updateTaskText = async (event) => {
-    const resp = await fetch ('http://localhost:8000/updateTask', {
+const updateTaskText = async (event) => {
+    const resp = await fetch (link + '/updateTask', {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -180,7 +181,7 @@ updateTaskText = async (event) => {
             text: event.target.value
         })
     });
-    const response = await fetch ('http://localhost:8000/allTasks', {
+    const response = await fetch (link + '/allTasks', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
